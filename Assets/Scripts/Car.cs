3eef078b95a14159b4a6b52c760fe347c1f2723a
Primary <yw3726@nyu.com>
2019-10-20 08:25:24 -0400

    Added CarGuiders (colliders only cars can collide to) Added CarDeleters Converted Box Spawners to Car Spawners.

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody MyRigidbody;
+
     void Start()
+    {
+
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        MyRigidbody.AddForce(transform.forward * 5);
     }
 }
