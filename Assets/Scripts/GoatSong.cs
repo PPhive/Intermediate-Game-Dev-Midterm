355e1cf6cca39b653a4d610a2d50ba5b06844e0a
Primary <yw3726@nyu.com>
2019-10-23 05:29:09 -0400

    UI, music, character, lantern and city updates. Final polishments.

---
+++ Assets/Scripts/GoatSong.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoatSong : MonoBehaviour
+{
+    public AudioSource MyAudioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyAudioSource = gameObject.GetComponent<AudioSource>();
+        MyAudioSource.PlayOneShot(MyAudioSource.clip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
